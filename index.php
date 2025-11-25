5c6c848297235a410e1d82d63e9e9e926f443d31
Lorelien Michiels <r0894804@student.thomasmore.be>
2025-11-25 15:10:11 +0100

    header added to index

--- index.php
+++ index.php
@@ -1,13 +1,39 @@
 <?php
-require_once 'classes/User.php';
-require_once 'classes/Book.php';
+include_once(__DIR__ . "/classes/Book.php");
+include_once(__DIR__ . "/classes/User.php");
 
 $user = new User();
 $book = new Book();
 $books = $book->getAllBooks();
-?>
 
-<?php include 'head.php'; ?>
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+?><!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>Boekwebshop</title>
+    <link rel="stylesheet" href="/webshop/style.css?v=<?= time() ?>">
+</head>
+<body>
+    <header>
+        <div class="header-container">
+            <div class="header-left">
+                <h1>Boekwebshop</h1>
+            </div>
+            <div class="header-right">
+                <?php if (isset($_SESSION['user_id'])): ?>
+                    <span>Hallo <?= htmlspecialchars($_SESSION['firstname']) ?></span>
+                    <a href="logout.php">Uitloggen</a>
+                <?php else: ?>
+                    <a href="login.php">Inloggen</a>
+                    <a href="register.php">Registreren</a>
+                <?php endif; ?>
+            </div>
+        </div>
+    </header>
 
 <main>
     <div class="book-list">
