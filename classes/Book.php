b673f0d0af8b27f1b6ca042178b1c900e2dd31cd
Lorelien Michiels <r0894804@student.thomasmore.be>
2025-11-25 15:10:43 +0100

    Book class setters + getters gemaakt

--- classes/Book.php
+++ classes/Book.php
@@ -1,15 +1,16 @@
 <?php
-require_once __DIR__ . '/db.php';
+include_once(__DIR__ . 'Db.php');
 
 class Book {
     private $db;
 
-    public function __construct() {
-        $this->db = (new Database())->pdo;
+    public function setDatabase(Database $database) {
+        $this->db = $database->getConnection();
     }
 
     public function getAllBooks() {
-        $stmt = $this->db->query("SELECT book_id AS id, title, author, price, cover_image FROM books");
+        $stmt = $this->db->query("SELECT book_id AS id, title, author, price, image FROM books");
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
+
 }
